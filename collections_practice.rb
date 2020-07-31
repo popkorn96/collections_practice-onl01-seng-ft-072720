309b9dcee0b28dc4274e7eaf37af0c517a5a0bd3
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-31 22:11:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -21,7 +21,7 @@ array [0], array [1], array[2] = array [0], array[2], array[1]
  def reverse_array(array)
    array.sort do |a,b|
      if a == b
-       0 
+       array.last 
        elsif a > b 
        -1
        elsif a < b 
