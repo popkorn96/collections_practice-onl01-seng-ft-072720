300b7023a558a2e7a4612b314204df611e0c5e35
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-31 22:27:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,6 +27,7 @@ array [0], array [1], array[2] = array [0], array[2], array[1]
    new_array = []
    array.each do |word| 
      new_array << "#{-word[2]}$"
+     counter += 1
  end
  new_array
  end
