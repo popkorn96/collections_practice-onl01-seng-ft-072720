a5a1e252d4ea2e96235d549f1dcafdbf4b335786
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-31 22:22:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -24,7 +24,8 @@ array [0], array [1], array[2] = array [0], array[2], array[1]
  
  def kesha_maker(array)
    new_array = []
-   array.each{|word| new_array << word[2] "$"}
+   array.each do |word| 
+     new_array << word[2] "$"
  end
  new_array
  end
