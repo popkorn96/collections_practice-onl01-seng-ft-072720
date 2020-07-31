2ac61242a9fd33e4d417c238c8571139037699ba
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-31 22:12:54 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,7 +22,7 @@ array [0], array [1], array[2] = array [0], array[2], array[1]
    array.sort do |a,b|
      if a == b
        0
-       elsif a > b a
+       elsif a > b 
        elsif a < b 
       1
      end
